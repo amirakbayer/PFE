f065a8ff887608e55566d21129ba3bfee92b6ed0
melekkatri <mkatri.mk@gmail.com>
2022-05-11 11:35:04 +0100

    html css

--- src/app/Front-Office/reclamation/reduced/reduced.component.ts
+++ src/app/Front-Office/reclamation/reduced/reduced.component.ts
@@ -24,7 +24,8 @@ export class ReducedComponent implements OnInit {
   id;
   affRecs;
   allRecs;
-  a
+  a=0;
+  
   constructor(private recService:RecServiceService, 
     private categorie:CategorieService , 
     private route: ActivatedRoute,
@@ -41,10 +42,11 @@ export class ReducedComponent implements OnInit {
     
     this.a=1;
   }
-
+T1;
   readRecs(){
     this.recService.getRec().subscribe((data) => {
-     this.allRecs = data;
+     this.T1 = data;
+     this.allRecs=Array.from({length:this.T1.length}, (_, k) => this.transformData1(k));
      console.log(this.allRecs)
     })    
   }
@@ -55,51 +57,151 @@ export class ReducedComponent implements OnInit {
       this.T=data;
       this.ownRecs=Array.from({length:this.T.length}, (_, k) => this.transformData(k));
       console.log("ownRecs is",this.ownRecs);
+      
       //this.allRecs = Array.of(this.allRecs);
-     }) 
+     
+  }) 
     //this.ownRecs=this.recService.getReclamsOfUser(this.id)
        
   }
+  T2;
   readAffRecs(){
     this.recService.getAffRecs(this.id)
     .subscribe((data) => {
-      this.affRecs = data 
-      console.log("affRecs is",this.ownRecs);
+      this.T2 = data; 
+      this.affRecs=Array.from({length:this.T2.length}, (_, k) => this.transformData2(k));
+      console.log("affRecs is",this.affRecs);
       //this.allRecs = Array.of(this.allRecs);
      })  
   }
   
-
+  processing=true;
   transformData(k: number):TableData{
     //it's better if I get the raw data here first so I can use this function to update 'Updates'
     var cat;
     var sCateg;
-    var processing=true;
+     var et;
     this.categorie.getSousCatDet(this.T[k].Id_sousCateg).subscribe((data) =>{
       sCateg=data;
     this.categorie.getCatDet(sCateg.id2).subscribe(
       
       {
         next:(res)=>{
-          cat= data.nom;
+          cat= res.nom;
       console.log('cat name is',cat)
-      processing=false;
-          return{
-            _id: this.T[k]._id,
-            date: this.T[k].date ,
-            categorie: cat ,
-            etat: this.etatN(this.T[k].id_etat)}
+      this.ownRecs[k].categorie=cat;
+      console.log('ownRecs after subscribe',this.ownRecs)
+      this.etat.getEtatDet(this.T[k].id_etat).subscribe((data)=>
+      { et=data.altNom;
+        this.ownRecs[k].etat=et;
+        console.log("etat is",et)
+        if(k==this.ownRecs.length-1){
+          this.processing=false;
+          console.log("ownrecs at last", this.ownRecs)
+        }
+      })
+      
+          
         }, error:()=>{
-          alert("échec lors de l'envoi de la réclamation");
+          alert("échec lors de chargement");
+          
         }
       }
       )
     })
-     return {
+    return {
       _id: this.T[k]._id,
       date: this.T[k].date ,
       categorie: '' ,
-      etat: this.etatN(this.T[k].id_etat)
+      etat: ''
+      
+     }
+   
+ }
+
+
+ processing1=true;
+  transformData1(k: number):TableData{
+    //it's better if I get the raw data here first so I can use this function to update 'Updates'
+    var cat;
+    var sCateg;
+    var et;
+    this.categorie.getSousCatDet(this.T1[k].Id_sousCateg).subscribe((data) =>{
+      sCateg=data;
+    this.categorie.getCatDet(sCateg.id2).subscribe(
+      
+      {
+        next:(res)=>{
+          cat= res.nom;
+      console.log('cat name is',cat)
+      this.allRecs[k].categorie=cat;
+      console.log('allRecs after subscribe',this.allRecs)
+      this.etat.getEtatDet(this.T1[k].id_etat).subscribe((data)=>
+      { et=data.nom;
+        this.allRecs[k].etat=et;
+        console.log("etat is",et)
+        if(k==this.allRecs.length-1){
+          this.processing1=false;
+          console.log("allrecs at last", this.allRecs)
+        }
+      })
+          
+        }, error:()=>{
+          alert("échec lors de chargement");
+          
+        }
+      }
+      )
+    })
+    return {
+      _id: this.T1[k]._id,
+      date: this.T1[k].date ,
+      categorie: '' ,
+      etat: ''
+      
+     }
+   
+ }
+
+
+ processing2=true;
+  transformData2(k: number):TableData{
+    //it's better if I get the raw data here first so I can use this function to update 'Updates'
+    var cat;
+    var sCateg;
+    var et; 
+    this.categorie.getSousCatDet(this.T2[k].Id_sousCateg).subscribe((data) =>{
+      sCateg=data;
+    this.categorie.getCatDet(sCateg.id2).subscribe(
+      
+      {
+        next:(res)=>{
+          cat= res.nom;
+      console.log('cat name is',cat)
+      this.affRecs[k].categorie=cat;
+      console.log('affRecs after subscribe',this.affRecs)
+      this.etat.getEtatDet(this.T2[k].id_etat).subscribe((data)=>
+      { et=data.nom;
+        this.affRecs[k].etat=et;
+        console.log("etat is",et)
+        if(k==this.affRecs.length-1){
+          this.processing2=false;
+          console.log("affrecs at last", this.affRecs)
+        }
+      })
+          
+        }, error:()=>{
+          alert("échec lors de chargement");
+          
+        }
+      }
+      )
+    })
+    return {
+      _id: this.T2[k]._id,
+      date: this.T2[k].date ,
+      categorie: '' ,
+      etat: ''
       
      }
    
@@ -109,9 +211,9 @@ export class ReducedComponent implements OnInit {
     this.a=x;
   }
   categ(idS){
-    this.categorie.getCatDet(idS).subscribe((data) =>{
-    return data.nom;
-    });
+    //this.categorie.getCatDet(idS).subscribe((data) =>{
+    //return data.nom;
+    //});
   }
   etatN(id){
     return this.etat.etatName(id)
