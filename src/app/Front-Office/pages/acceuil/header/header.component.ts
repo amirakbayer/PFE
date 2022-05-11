fc46bc0a36282486db4772ae11fc174f726487ad
amira <amira.benkbayer@gmail.com>
2022-05-11 12:46:27 +0200

    11-05

--- src/app/Front-Office/pages/acceuil/header/header.component.ts
+++ src/app/Front-Office/pages/acceuil/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   raw7njh: string = ' '
   constructor() { }
-
+//comment
   ngOnInit(): void {
   }
 
